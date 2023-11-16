8b05778e8ecd28c0f5952ffd95a94fc4b7bad430
Pep Vilarós <vilarospro@gmail.com>
2023-11-16 19:58:42 +0100

    Configurate ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'standard',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -16,5 +17,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-  },
+  }
 }
